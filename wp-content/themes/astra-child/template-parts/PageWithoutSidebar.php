ef476b5863f675ec2b568502856752d02083e4a8
anilsingh4580 <anil@techtonic.asia>
2021-04-20 21:38:32 +0530

    template done

---
+++ wp-content/themes/astra-child/template-parts/PageWithoutSidebar.php
@@ -0,0 +1,150 @@
+<?php /* Template Name: PageWithoutSidebar */
+get_header();
+?>
+
+
+
+<section class="search_box_main">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-10 m-auto">
+                    <div class="row">
+                        <div
+                            class="col-md-8 col-lg-8 col-sm-11 col-11 mx-sm-auto text-sm-center mx-lg-auto search_box_mobile">
+                            <div class="serchbox-content">
+                                <h2>Be unstoppable</h2>
+                                <div class="search-box">
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend catalog">
+                                            <span class="input-group-text input_group">catalog</span>
+                                        </div>
+                                        <input type="text" class="form-control shadow-none set_outline"
+                                            aria-label="Amount (to the nearest dollar)"
+                                            placeholder="Enter ISBN, Author, or Title. ISBN Recommended" />
+                                        <div class="input-group-append">
+                                            <span class="input-group-text search_icon">
+                                                <i class="fa fa-search"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-11 mx-sm-auto text-sm-center col-11 search_box_mobile">
+                            <div class="search-box-img">
+                                <img src="./assets/home/smiling-indian-young-woman-college-university-school-student.png"
+                                    alt="smiling indian young woman college university school student"
+                                    class="img-fluid" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="explore_main">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-11 m-auto">
+                    <div class="all_section_heading">
+                        <h2>explore</h2>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-3 col-sm-6 col-6 text-center">
+                            <a href="./catalog-higher-education.html">
+                                <div class="expolre_child">
+                                    <div class="explore_img">
+                                        <img src="./assets/home/01_Higher-Ed@2x.png" alt="Higher Education"
+                                            class="img-fluid">
+                                    </div>
+                                    <h4>Higher Education</h4>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-6 text-center">
+                            <div class="expolre_child">
+                                <div class="explore_img">
+                                    <img src="./assets/home/02_Test-Prep@2x.png" alt="Test Prep" class="img-fluid">
+                                </div>
+                                <h4>Test Prep</h4>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-6 text-center">
+                            <div class="expolre_child">
+                                <div class="explore_img">
+                                    <img src="./assets/home/03_ELT-School@2x.png" alt="ELT/School" class="img-fluid">
+                                </div>
+                                <h4>ELT/School</h4>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-6 text-center">
+                            <div class="expolre_child">
+                                <div class="explore_img">
+                                    <img src="./assets/home/04_Library-Reference@2x.png" alt="Library Reference"
+                                        class="img-fluid">
+                                </div>
+                                <h4>Library Reference</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="watch_section">
+        <div class="container-md">
+            <div class="row">
+                <div class="col-md-6 col-lg-6 col-11 col-sm-11 mx-sm-auto mx-auto mr-md-auto">
+                    <div class="watch_header">
+                        <h2>For innovative teaching, learning and research solutions</h2>
+                        <p>
+                            Cengage Learning India provides learning solutions to higher
+                            education, professional, library reference and school markets in
+                            the Indian sub-continent. We, at Cengage Learning India, develop
+                            and publish products to meet the constantly changing needs of
+                            our educational, professional, trade and library markets.
+                        </p>
+                        <div class="py-md-4 watch_video_button">
+                            <button type="button" class="btn btn-outline-light text-uppercase py-3 px-4">
+                                Watch THE VIDEOS
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5 col-lg-6 col-11 col-sm-11 mx-sm-auto mx-auto text-sm-right">
+                    <div class="watch_image">
+                        <img src="./assets/mask-group.png" alt="mask group" class="img-fluid" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php get_footer(); ?>
